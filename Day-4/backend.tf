5505caf77098f42af56099eabd69dcc36e2cbd14
Prajjwal <prajjwalpawar1@gmail.com>
2024-04-19 12:22:39 +0530

    made changes to S3 bucket

--- Day-4/backend.tf
+++ Day-4/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "abhishek-s3-demo-xyz" # change this
-    key            = "abhi/terraform.tfstate"
+    bucket         = "praj-s3-demo-xyz" # change this
+    key            = "praj/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
