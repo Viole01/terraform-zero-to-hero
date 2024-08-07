c2fd0fc579778055aa972b9812bd02d0976ddc29
Prajjwal <prajjwalpawar1@gmail.com>
2024-08-07 16:53:47 +0530

    learned terraform vault and secret menagement

--- Day-7/main.tf
+++ Day-7/main.tf
@@ -3,21 +3,21 @@ provider "aws" {
 }
 
 provider "vault" {
-  address = "<>:8200"
+  address          = "http://<>:8200"
   skip_child_token = true
 
   auth_login {
     path = "auth/approle/login"
 
     parameters = {
-      role_id = "<>"
+      role_id   = "<>"
       secret_id = "<>"
     }
   }
 }
 
 data "vault_kv_secret_v2" "example" {
-  mount = "secret" // change it according to your mount
+  mount = "secret"      // change it according to your mount
   name  = "test-secret" // change it according to your secret
 }
 
@@ -26,7 +26,7 @@ resource "aws_instance" "my_instance" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "test"
-    Secret = data.vault_kv_secret_v2.example.data["foo"]
+    name   = "test"
+    Secret = data.vault_kv_secret_v2.example.data["username"]
   }
 }
