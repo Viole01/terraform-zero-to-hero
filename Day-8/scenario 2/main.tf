0dd911db45807e832d25f859b7868eee9e79c77f
Prajjwal <prajjwalpawar1@gmail.com>
2024-08-08 13:18:02 +0530

    drift detection

---
+++ Day-8/scenario 2/main.tf
@@ -0,0 +1,9 @@
+# DRIFT DETECTION
+
+# 1st method is using command
+# terraform refresh 
+# this command refreshes the statefile
+
+# 2nd way has 2 more parts
+# 1st one is use strict IAM roles
+# 2nd one is use apply audit logging
